f32c0c8714a96dca850e0cc81ecae5e4fe459d6a
Alexander Chabo <alexander.chabo@gmail.com>
2023-06-02 14:10:38 +0200

    Switched to utilising segment.io

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import { Analytics } from "@vercel/analytics/react";
+import Script from "next/script";
 
 import { NavBar } from "@/components/Navbar/NavBar";
 import "./globals.css";
@@ -14,10 +14,18 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Script
+        id="segment.io"
+        dangerouslySetInnerHTML={{
+          __html: `!function(){var analytics=window.analytics=window.analytics||[];if(!analytics.initialize)if(analytics.invoked)window.console&&console.error&&console.error("Segment snippet included twice.");else{analytics.invoked=!0;analytics.methods=["trackSubmit","trackClick","trackLink","trackForm","pageview","identify","reset","group","track","ready","alias","debug","page","once","off","on","addSourceMiddleware","addIntegrationMiddleware","setAnonymousId","addDestinationMiddleware"];analytics.factory=function(e){return function(){var t=Array.prototype.slice.call(arguments);t.unshift(e);analytics.push(t);return analytics}};for(var e=0;e<analytics.methods.length;e++){var key=analytics.methods[e];analytics[key]=analytics.factory(key)}analytics.load=function(key,e){var t=document.createElement("script");t.type="text/javascript";t.async=!0;t.src="https://cdn.segment.com/analytics.js/v1/" + key + "/analytics.min.js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(t,n);analytics._loadOptions=e};analytics._writeKey="${process.env.SEGMENT_WRITE_KEY}";;analytics.SNIPPET_VERSION="4.15.3";
+          analytics.load("${process.env.SEGMENT_WRITE_KEY}");
+          analytics.page();
+          }}();`,
+        }}
+      />
       <body className={inter.className}>
         <NavBar />
         <main className={styles.main}>{children}</main>
-        <Analytics />
       </body>
     </html>
   );
