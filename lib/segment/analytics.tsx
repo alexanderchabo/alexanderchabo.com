e43af5786f2baef02e7457c00f588d9fb31b5b04
Alexander Chabo <alexander.chabo@gmail.com>
2023-06-02 16:53:10 +0200

    Removed initial pageview

--- lib/segment/analytics.tsx
+++ lib/segment/analytics.tsx
@@ -4,14 +4,9 @@ if (!process.env.NEXT_PUBLIC_SEGMENT_WRITE_KEY) {
   throw new Error("Missing NEXT_PUBLIC_SEGMENT_WRITE_KEY");
 }
 
-const analytics = AnalyticsBrowser.load(
-  {
-    writeKey: process.env.NEXT_PUBLIC_SEGMENT_WRITE_KEY,
-  },
-  {
-    initialPageview: true,
-  }
-);
+const analytics = AnalyticsBrowser.load({
+  writeKey: process.env.NEXT_PUBLIC_SEGMENT_WRITE_KEY,
+});
 
 export const trackPage = (url: string) => {
   analytics.page();
