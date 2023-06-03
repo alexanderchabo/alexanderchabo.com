c7c531cfce7407b348a1dab09bcb7a42fa8f3e76
Alexander Chabo <alexander.chabo@gmail.com>
2023-06-03 14:37:55 +0200

    No initialt page tracking

--- lib/segment/snippet.ts
+++ lib/segment/snippet.ts
@@ -3,6 +3,7 @@ import snippet from "@segment/snippet";
 export const renderSnippet = () => {
   const options = {
     apiKey: process.env.NEXT_PUBLIC_SEGMENT_WRITE_KEY,
+    page: false,
   };
 
   if (process.env.NODE_ENV === "development") {
