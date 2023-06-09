c8651e19c9b8cd08bafe9ec903d9b99c631ab726
Alexander Chabo <alexander.chabo@gmail.com>
2023-06-09 14:32:32 +0200

    Switched to strategy "afterInteractive"

--- lib/segment/Analytics.tsx
+++ lib/segment/Analytics.tsx
@@ -3,19 +3,18 @@
 import Script from "next/script";
 import { renderSnippet } from "@/lib/segment/snippet";
 import { trackPage } from "@/lib/segment/utils";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
+
 import { usePathname, useSearchParams } from "next/navigation";
 
 export const Analytics = () => {
-  const [ready, setReady] = useState(false);
-
   const pathname = usePathname();
   const searchParams = useSearchParams();
 
   useEffect(() => {
     const url = pathname + searchParams.toString();
     trackPage(url);
-  }, [ready, pathname, searchParams]);
+  }, [pathname, searchParams]);
 
   return (
     <Script
@@ -23,8 +22,7 @@ export const Analytics = () => {
       dangerouslySetInnerHTML={{
         __html: renderSnippet(),
       }}
-      strategy="lazyOnload"
-      onReady={() => setReady(true)}
+      strategy="afterInteractive"
     />
   );
 };
--- lib/segment/utils.ts
+++ lib/segment/utils.ts
@@ -1,6 +1,7 @@
 export const trackPage = (_url: string) => {
   // TODO: type this
   if ((global as any).analytics) {
+    console.log("hello");
     (global as any).analytics.page();
   }
 };
